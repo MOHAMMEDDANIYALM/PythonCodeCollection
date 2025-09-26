66c1c820fe73d702f0f479046d9565ad29ac04d4
Mohammed Daniyal <certifieddaniyal@gmail.com>
2025-09-26 22:44:07 +0530

    Update Flask project with working routes and HTML templates

--- projects/marks_app/app.py
+++ projects/marks_app/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")  #home page 
+@app.route("/")
 def home():
-    return render_template("index.html")  #show index.html
- 
-if __name__=="__main__":
-    app.run(debug = True)
+    return render_template("add.html")  
+
+@app.route("/submit_marks", methods=["POST"])
+def submit_marks():
+    student_name = request.form['student_name']
+    subject = request.form['subject']
+    marks = request.form['marks']
+    return f"Received: {student_name} - {subject} - {marks}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
